62fc38c54ea2383c1416cccc601f16cfa0236f5a
firas foued <firas.fouad@isimg.tn>
2024-08-25 23:43:25 +0100

    voice_chat

--- voice_chat/lib/home_page.dart
+++ voice_chat/lib/home_page.dart
@@ -5,7 +5,8 @@ import 'package:voice_chat/pallette.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
+      
+      
   @override
   State<HomePage> createState() => _HomePageState();
 }
